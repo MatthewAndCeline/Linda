52f576c1d4a7689239fa7be7790f445291516727
Matthix <matthew.hixson1@gmail.com>
2015-02-24 11:20:38 +0100

    small mod

--- c_h2O.py
+++ c_h2O.py
@@ -9,7 +9,7 @@ linda.connect()
 ts = linda.universe._rd(("TupleSpace drainage", linda.TupleSpace))[1]
 
 for i in range (0,10):
-	ts._out(("Niveau", 60.6))	
+	ts._out(("Niveau_H2O", 60.6))	
 	print("C_H20")
 	time.sleep(1)
 
