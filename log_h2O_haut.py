b173047a79cad108ce9489ab3c21de8444a34026
Céline de Roland <celinederoland@itpassion.info>
2015-04-26 10:38:51 +0200

    compte rendu suite

--- log_h2O_haut.py
+++ log_h2O_haut.py
@@ -27,7 +27,6 @@ Label(fenetre,textvariable=message).pack(padx=5,pady=5)
 def maj():
 	message.set("attente niveau haut")
 	ts._in(("H2O_haut_detecté",))
-	#print("logique H2O haut a détecté un
 	valeur_CH4 = ts._rd(("Niveau_CH4",float))[1]
 	valeur_CO = ts._rd(("Niveau_CO",float))[1]
 	if (valeur_CH4 < seuil_CH4 and valeur_CO < seuil_CO):
