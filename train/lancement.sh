774e13fda072b087eff6c365dc7b29e9a74879f5
Remi Mollard <remimollard38@gmail.com>
2015-04-27 19:49:29 +0200

    Modif lancement

--- train/lancement.sh
+++ train/lancement.sh
@@ -8,9 +8,14 @@ echo "Lancement du capteur demandes de sorties !"
 ./c_demandes_sorties.py&
 
 echo "Lancement des Trains !"
-for n in $1
-do
-	numTrain= n * 50
-	./train.py n&
-done
+#for n in $1
+#do
+#	numTrain= n * 50
+#	./train.py numTrain&
+#done
+./train.py 0&
+./train.py 100&
+./train.py 200&
+./train.py 300&
+./train.py 400&
 echo "Fait."
