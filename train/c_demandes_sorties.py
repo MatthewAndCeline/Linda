49434c92c1e20554c292c143f1de9f7259d8284a
Remi Mollard <remimollard38@gmail.com>
2015-04-27 19:39:19 +0200

    Modif c_demandes virgules

--- train/c_demandes_sorties.py
+++ train/c_demandes_sorties.py
@@ -17,14 +17,14 @@ temps = ts._rd(("Temps_Rafraichissement",int))[1]
 # On crée une fenêtre, racine de notre interface
 fenetre = Tk()
 fenetre.title("Capt. Demandes Sorties")
-fenetre.geometry('150x50+500+200')
+fenetre.geometry('150x50+500+300')
 Label(fenetre,text="C.D.S").pack(padx=5,pady=5)
 info = StringVar()
 Label(fenetre,textvariable=info).pack(padx=5,pady=5)
 
 # Fonction de mise à jour à réaliser en permanence
 def maj():
-	ts._in(("demande_sorties"))
+	ts._in(("demande_sorties"),)
 	val_nb_demandes_sorties = ts._in(("nombre_demandes_sorties",int))[1]
 	val_nb_demandes_sorties += 1
 	ts._out(("nombre_demandes_sorties",val_nb_demandes_sorties))
