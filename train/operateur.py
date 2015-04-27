644e751ca8a7e680c5c264c5d1b8c11660f989ee
Céline de Roland <celinederoland@itpassion.info>
2015-04-27 18:32:14 +0200

    en train de faire le train

--- train/operateur.py
+++ train/operateur.py
@@ -13,6 +13,8 @@ linda.universe._out(("TupleSpace train", ts))
 #Paramétrage du système
 temps = 500
 ts._out(("Temps_Rafraichissement",temps))
+ts._out(("nombre_demandes_sorties",0))
+ts._out(("nombre_demandes_sorties",0))
 
 # On crée une fenêtre, racine de notre interface
 fenetre = Tk()
